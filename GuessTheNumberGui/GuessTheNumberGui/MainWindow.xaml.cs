e1351534e75990ec063ea32bdb9ab6542aef40f2
Izabela <isabs@cmoki.pl>
2016-02-27 23:39:46 +0100

    many changes, created viewmodel, its not complete yet but it shoud display well... Now I need observers to notify about changes made in vm... many of them...

--- GuessTheNumberGui/GuessTheNumberGui/MainWindow.xaml.cs
+++ GuessTheNumberGui/GuessTheNumberGui/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using GuessTheNumber;
 using GuessTheNumber.CompareStrategies;
 using GuessTheNumberGui.commands;
@@ -24,67 +15,12 @@ namespace GuessTheNumberGui
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<CompareStrategy> Modes { get; set; }
-        public string SumLabel { get; set; }
-        private Number Number { get; set; }
-
-        public ICommand StartClickCommand { get { return new StartCommand(StartGame, () => true); } }
-
         public MainWindow()
         {
-            Modes = new List<CompareStrategy> {new CompareDigits(), new CompareAll()};
-            SumLabel = string.Empty;
+/*            Modes = new List<CompareStrategy> { new CompareDigits(), new CompareAll() };
+            SumLabel = string.Empty;*/
             InitializeComponent();
-            this.DataContext = this;
-        }
 
-        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void StartGame()
-        {
-            var value = new[] { SetUninitializedNumeric(txtNumber1), SetUninitializedNumeric(txtNumber2),
-                SetUninitializedNumeric(txtNumber3), SetUninitializedNumeric(txtNumber4) };
-
-            CompareStrategy comparer; 
-
-            if((CompareStrategy)cmbMode.SelectedItem == null) comparer = new CompareAll();
-            else comparer = (CompareStrategy)cmbMode.SelectedItem;
-
-            if (Number == null || Number.Compare(value))
-            {
-                var withSum = chkWithSum.IsChecked ?? false; 
-
-                btnStart.Visibility = Visibility.Hidden;
-                btnCheck.Visibility = Visibility.Visible;
-
-                Number = new Number(4, comparer);
-
-                Number.Start();
-
-                cmbMode.IsReadOnly = true;
-
-                if (withSum)
-                {
-                    SumLabel = "Sum of digits should be " + Number.GetSumOfDigits();
-                }
-
-                //Number = new Number(cmbMode.GetValue());
-            }
-            else
-            {
-                throw new Exception("Clicked Start Button, which cannot exist...");
-            }
-        }
-
-        private int SetUninitializedNumeric(IntegerUpDown txtNumber)
-        {
-            if (txtNumber.Value == null)
-                txtNumber.Value = 0;
-
-            return txtNumber.Value.GetValueOrDefault();
         }
     }
 }
---
+++ GuessTheNumberGui/GuessTheNumberGui/MainWindowViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using GuessTheNumber;
+using GuessTheNumber.CompareStrategies;
+using GuessTheNumberGui.commands;
+using Xceed.Wpf.Toolkit;
+
+namespace GuessTheNumberGui
+{
+    public class MainWindowViewModel
+    {
+        public int[] CurrentNumber { get; set; }
+
+        public int FirstDigit
+        {
+            get { return CurrentNumber[0]; } 
+            set { CurrentNumber[0] = value; }
+        }
+
+        public int SecondDigit
+        {
+            get { return CurrentNumber[1]; }
+            set { CurrentNumber[1] = value; }
+        }
+
+        public int ThirdDigit
+        {
+            get { return CurrentNumber[2]; }
+            set { CurrentNumber[2] = value; }
+        }
+
+        public int FourthDigit
+        {
+            get { return CurrentNumber[3]; }
+            set { CurrentNumber[3] = value; }
+        }
+
+        public List<CompareStrategy> Modes { get; set; }
+        public CompareStrategy ActualSelectedMode { get; set; }
+        public bool WithSum { get; set; }
+
+        public Visibility StartVisibility { get; set; }
+        public Visibility CheckVisibility { get; set; }
+
+
+        public string SumLabel { get; set; }
+        private Number Number { get; set; }
+
+        public ICommand StartClickCommand { get { return new StartCommand(StartGame, () => true); } }
+
+        public MainWindowViewModel()
+        {
+            CurrentNumber = new[] {0, 0, 0, 0};
+            Modes = new List<CompareStrategy>{ new CompareDigits(), new CompareAll() };
+            ActualSelectedMode = Modes[0];
+            SumLabel = string.Empty;
+            StartVisibility = Visibility.Visible;
+            CheckVisibility = Visibility.Hidden;
+
+        }
+
+        private void StartGame()
+        {
+            if (Number == null || Number.Compare(CurrentNumber))
+            { // starting new game! else something bad is happening..
+                StartVisibility = Visibility.Hidden;
+                CheckVisibility = Visibility.Visible;
+
+                Number = new Number(4, ActualSelectedMode);
+
+                Number.Start();
+
+/*                cmbMode.IsReadOnly = true;
+
+                if (withSum)
+                {
+                    SumLabel = "Sum of digits should be " + Number.GetSumOfDigits();
+                }*/
+
+                //Number = new Number(cmbMode.GetValue());
+            }
+            else
+            {
+                throw new Exception("Clicked Start Button, which cannot exist...");
+            }
+        }
+    }
+}
