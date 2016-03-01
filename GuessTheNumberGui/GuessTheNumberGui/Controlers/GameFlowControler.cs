ac9e6fbaf1fb10110f780b03d3a98ee4ef61b4f3
Izabela <isabs@cmoki.pl>
2016-03-01 22:06:58 +0100

    Refactoring

---
+++ GuessTheNumberGui/GuessTheNumberGui/Controlers/GameFlowControler.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using GuessTheNumber;
+using GuessTheNumberGui.commands;
+
+namespace GuessTheNumberGui.Controlers
+{
+    public class GameFlowControler : ViewModelBase
+    {
+        private CurrentNumberControler _currentNumberControler;
+        private ComboControler _comboControler;
+        private SumModeControler _sumModeControler;
+
+        private Visibility _startVisibility;
+        private Visibility _checkVisibility;
+        private bool _gameStarted;
+        private string _resultsText;
+
+        private Number Number { get; set; }
+
+        public ICommand StartClickCommand { get { return new RelayCommand(StartGame, () => true); } }
+        public ICommand CheckClickCommand { get { return new RelayCommand(Check, () => true); } }
+
+        public Visibility StartVisibility
+        {
+            get { return _startVisibility; }
+            set
+            {
+                _startVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Visibility CheckVisibility
+        {
+            get { return _checkVisibility; }
+            set
+            {
+                _checkVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public GameFlowControler(CurrentNumberControler currentNumberControler, ComboControler comboControler, SumModeControler sumModeControler)
+        {
+            StartVisibility = Visibility.Visible;
+            CheckVisibility = Visibility.Hidden;
+            _currentNumberControler = currentNumberControler;
+            _comboControler = comboControler;
+            _sumModeControler = sumModeControler;
+            GameEnded = true;
+        }
+
+        public void StartGame()
+        {
+            if (Number == null || Number.Compare(_currentNumberControler.CurrentNumber))
+            {
+                StartVisibility = Visibility.Hidden;
+                CheckVisibility = Visibility.Visible;
+
+                Number = new Number(4, _comboControler.ActualSelectedMode);
+
+                Number.Start();
+
+                GameEnded = false;
+
+                if (_sumModeControler.WithSum)
+                {
+                    _sumModeControler.SumLabel = string.Format("Sum of digits should be " + Number.GetSumOfDigits());
+                }
+            }
+            else
+            {
+                throw new Exception("Clicked Start Button, which cannot exist...");
+            }
+        }
+
+        public void Check()
+        {
+
+            ResultsText = Number.CompareText(_currentNumberControler.CurrentNumber);
+
+            if (Number.Compare(_currentNumberControler.CurrentNumber))
+            {
+                StartVisibility = Visibility.Visible;
+                CheckVisibility = Visibility.Hidden;
+
+                GameEnded = true;
+                _sumModeControler.SumLabel = string.Empty;
+
+                Number = null;
+            }
+        }
+
+        
+
+        public bool GameEnded
+        {
+            get { return _gameStarted; }
+            set
+            {
+                _gameStarted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ResultsText
+        {
+            get { return _resultsText; }
+            set
+            {
+                _resultsText = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
--- GuessTheNumberGui/GuessTheNumberGui/MainWindowViewModel.cs
+++ GuessTheNumberGui/GuessTheNumberGui/MainWindowViewModel.cs
@@ -1,123 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 using GuessTheNumber;
-using GuessTheNumber.CompareStrategies;
 using GuessTheNumberGui.commands;
 using GuessTheNumberGui.Controlers;
-using Xceed.Wpf.Toolkit;
 
 namespace GuessTheNumberGui
 {
     public class MainWindowViewModel : ViewModelBase
-    {
-        private Visibility _startVisibility ;
-        private Visibility _checkVisibility;
-        private bool _gameStarted;
-        private string _resultsText;
+    {   // TODO: further refactoring
+        // TODO: Adding button to automatic setting up-downs based on desired sum
+        // TODO: Super flashy congratulations view
+        // TODO: Number of wins, steps, ranking; this must be saved to file
+
+
 
         public CurrentNumberControler CurrentNumberControler { get; private set; }
         public ComboControler ComboControler { get; set; }
         public SumModeControler SumModeControler { get; set; }
 
-        public Visibility StartVisibility
-        {
-            get { return _startVisibility; }
-            set
-            {
-                _startVisibility = value;
-                OnPropertyChanged();
-            }
-        }
+        public GameFlowControler GameFlowControler { get; set; }
 
-        public Visibility CheckVisibility
-        {
-            get { return _checkVisibility; }
-            set
-            {
-                _checkVisibility = value;
-                OnPropertyChanged();
-            }
-        }
 
-        private Number Number { get; set; }
 
-        public bool GameEnded
-        {
-            get { return _gameStarted; }
-            set
-            {
-                _gameStarted = value;
-                OnPropertyChanged();
-            }
-        }
 
-        public string ResultsText
-        {
-            get { return _resultsText;  }
-            set
-            {
-                _resultsText = value;
-                OnPropertyChanged();
-            }
-        }
 
-        public ICommand StartClickCommand { get { return new RelayCommand(StartGame, () => true); } }
-        public ICommand CheckClickCommand { get { return new RelayCommand(Check, () => true); } }
 
         public MainWindowViewModel()
         {
             CurrentNumberControler = new CurrentNumberControler();
             ComboControler = new ComboControler();
             SumModeControler = new SumModeControler();
-          
-            StartVisibility = Visibility.Visible;
-            CheckVisibility = Visibility.Hidden;
-            GameEnded = true;
+
+            GameFlowControler = new GameFlowControler(CurrentNumberControler, ComboControler, SumModeControler);
+
+
         }
 
-        public void StartGame()
-        {
-            if (Number == null || Number.Compare(CurrentNumberControler.CurrentNumber))
-            {
-                StartVisibility = Visibility.Hidden;
-                CheckVisibility = Visibility.Visible;
 
-                Number = new Number(4, ComboControler.ActualSelectedMode);
-
-                Number.Start();
-
-                GameEnded = false;
-
-                if (SumModeControler.WithSum)
-                {
-                    SumModeControler.SumLabel = string.Format("Sum of digits should be " + Number.GetSumOfDigits());
-                }
-            }
-            else
-            {
-                throw new Exception("Clicked Start Button, which cannot exist...");
-            }
-        }
-
-        public void Check()
-        {
-
-            ResultsText = Number.CompareText(CurrentNumberControler.CurrentNumber);
-
-            if (Number.Compare(CurrentNumberControler.CurrentNumber))
-            {
-                StartVisibility = Visibility.Visible;
-                CheckVisibility = Visibility.Hidden;
-
-                GameEnded = true;
-                SumModeControler.SumLabel = string.Empty;
-
-                Number = null;
-            }
-        }
     }
-
-    //add congratulations view with fireworks
+    
 }
